bdfce88ef4f4e4058d7cd2d186301ccb200c2ba4
Javier Moreno Rodríguez <javiemr0@gmail.com>
2023-10-20 10:03:12 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'about',
     loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
   },
+  {
+    path: 'crud',
+    loadChildren: () => import('./crud/crud.module').then( m => m.CrudPageModule)
+  },
   
 ];
 
