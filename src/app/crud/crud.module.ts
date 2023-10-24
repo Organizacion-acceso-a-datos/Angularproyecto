8b7da54a25e9b713c0efe5e0c51d27580a0f5e1f
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2023-10-24 10:25:37 +0200

    implementar shared-module en las paginas

--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { CrudPageRoutingModule } from './crud-routing.module';
 
 import { CrudPage } from './crud.page';
 import { NavegadorComponent } from '../navegador/navegador.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     CrudPageRoutingModule
   ],
   declarations: [CrudPage,],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { NavegadorComponent } from '../navegador/navegador.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage, NavegadorComponent],
