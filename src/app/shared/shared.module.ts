afd9414ebfec5078e862936a427070822e03b5cb
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2023-10-31 11:14:29 +0100

    Añadir service en modulo compartido

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { NavegadorComponent } from '../navegador/navegador.component';
 import { RouterModule } from '@angular/router';
 import { AutoresComponent } from '../about/autores/autores.component';
+import { CocheService } from '../crud/coche.service';
 
 
 @NgModule({
-  declarations: [NavegadorComponent, AutoresComponent],
+  declarations: [NavegadorComponent, AutoresComponent, CocheService],
   imports: [
     CommonModule, IonicModule, FormsModule, RouterModule
   ],
-  exports: [CommonModule, IonicModule, FormsModule, NavegadorComponent, AutoresComponent]
+  exports: [CommonModule, IonicModule, FormsModule, NavegadorComponent, AutoresComponent, CocheService]
 })
 export class SharedModule { }
